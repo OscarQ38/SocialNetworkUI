b5f4b680de700c5bb8bebb88dbf20009d1a5c722
Gerardo M <gmayboca@gmail.com>
2021-04-05 19:48:45 -0700

    Se cambiaron los parametros del register

--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -5,21 +5,40 @@ Usuario usuarioFromJson(String str) => Usuario.fromJson(json.decode(str));
 String usuarioToJson(Usuario data) => json.encode(data.toJson());
 
 class Usuario {
-    Usuario({
-        this.uid,
-        this.nombre
-    });
+  Usuario({
+    this.uid,
+    this.nombre,
+    this.email,
+    this.nombreCompleto,
+    this.apellidoMaterno,
+    this.apellidoPaterno,
+    this.password,
+  });
 
-    String uid;
-    String nombre;
+  String uid;
+  String email;
+  String nombre;
+  String nombreCompleto;
+  String apellidoMaterno;
+  String apellidoPaterno;
+  String password;
 
-    factory Usuario.fromJson(Map<String, dynamic> json) => Usuario(
-        uid: json["uid"],
-        nombre: json["nombre"],
-    );
+  factory Usuario.fromJson(Map<String, dynamic> json) => Usuario(
+      uid: json["uid"],
+      nombre: json["nombre"],
+      email: json["email"],
+      nombreCompleto: json["nombreCompleto"],
+      apellidoMaterno: json["apellidoMaterno"],
+      apellidoPaterno: json["apellidoPaterno"],
+      password: json["password"]);
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "uid": uid,
         "nombre": nombre,
-    };
+        "email": email,
+        "nombreCompleto": nombreCompleto,
+        "apellidoMaterno": apellidoMaterno,
+        "apellidoPaterno": apellidoPaterno,
+        "password": password
+      };
 }
